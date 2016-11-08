74852c0407654029eb7429c89b12d14d9ae811ab
Servio Mora <servio.mora19@gmail.com>
2016-11-08 17:04:11 -0500

    gulp workfow done

--- gulpfile.js
+++ gulpfile.js
@@ -9,22 +9,26 @@ gulp.task('scripts', function () {
   //main scripts
   gulp.src('./src/js/**/*.js')
   .pipe(uglify())
-  .pipe(concat('app.min.js'))
+  .pipe(concat('scripts.min.js'))
   .pipe(gulp.dest('./dist/js'));
 
   //make 1 vendor file
   gulp.src('./src/vendor/**/*.js')
   .pipe(uglify())
-  .pipe(concat("vendor.js"))
+  .pipe(concat('vendor.js'))
   .pipe(gulp.dest('./dist/js'))
 });
 
 gulp.task('sass', function () {
-  //sass to css
+  gulp.src('./src/sass/*.sass')
+  .pipe(sass({ style: 'compressed' }))
+  .pipe(concat('style.css'))
+  .pipe(gulp.dest('./dist/css'))
 });
 
 gulp.task('watch', function () {
-  //watch files and run tasks
+  gulp.watch('./src/sass/**/*.sass', ['sass'])
+  gulp.watch('./src/js/**/*.js', ['scripts'])
 });
 
-gulp.task('default', ['']);
+gulp.task('default', ['watch', 'scripts', 'sass']);
