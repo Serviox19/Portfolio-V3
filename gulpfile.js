167aa12d08c59b86664ae7e0e3c79b3c9117fedd
Servio Mora <servio.mora19@gmail.com>
2016-11-08 13:22:06 -0500

    more additions and setting up gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
-var gulp = require('gulp');
+var gulp   = require('gulp'),
+    concat = require('gulp-concat'),
+    sass   = require('gulp-sass'),
+    uglify = require('gulp-uglify'),
+    watch  = require('gulp-watch');
+
+
+gulp.task('scripts', function () {
+  //main scripts
+  gulp.src('./src/js/**/*.js')
+  .pipe(uglify())
+  .pipe(concat('app.min.js'))
+  .pipe(gulp.dest('./dist/js'));
+
+  //make 1 vendor file
+  gulp.src('./src/vendor/**/*.js')
+  .pipe(uglify())
+  .pipe(concat("vendor.js"))
+  .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('sass', function () {
+  //sass to css
+});
+
+gulp.task('watch', function () {
+  //watch files and run tasks
+});
+
+gulp.task('default', ['']);
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,3 @@
 $(document).ready(function () {
-
-  alert('Test');
-
+  alert("Hello World");
 });
