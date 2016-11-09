c78ac3ee69998365592713a4588bee5d8cf7eb0d
Servio Mora <servio.mora19@gmail.com>
2016-11-08 23:39:17 -0500

    added scroll to top button with functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-  
+
+  var offset = 300,
+    offset_opacity = 1200,
+    scroll_duration = 700,
+    $back_to_top = $('.top');
+
+
+      $(window).scroll(function(){
+    		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('top-is-visible')
+         : $back_to_top.removeClass('top-is-visible top-fade-out');
+    		if( $(this).scrollTop() > offset_opacity ) {
+    			$back_to_top.addClass('top-fade-out');
+    		}
+	     });
+
+      $('.top').click(function(event) {
+        event.preventDefault();
+        $('body, html').animate({
+          scrollTop: 0,
+        }, scroll_duration);
+      });
 });
