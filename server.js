8ba0d54d7d607519c59eca3a723a34814ce8eb57
Servio Mora <servio.mora19@gmail.com>
2016-11-12 22:58:23 -0500

    implemented server

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+var PORT = process.env.PORT || 3000;
+
+
+app.use(logger('dev'));
+app.use(express.static("dist"));
+app.use(express.static("src"));
+app.use("/dist", express.static(__dirname + "/dist"));
+app.use("/src", express.static(__dirname + "/src"));
+app.use("/images", express.static(__dirname + "/images"));
+app.use("/bower_components", express.static(__dirname + "/bower_components"));
+
+app.get('*', function (req, res) {
+  res.sendFile(process.cwd() + 'src/index.html');
+});
+
+
+app.listen(PORT, function (req, res) {
+  console.log('Listening on PORT: ' + PORT);
+});
