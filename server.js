2005d22d9403185e37fb7ce7784bb5ddd7f99188
Servio Mora <servio.mora19@gmail.com>
2016-11-14 16:29:20 -0500

    deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use("/images", express.static(__dirname + "/images"));
 app.use("/bower_components", express.static(__dirname + "/bower_components"));
 
 app.get('*', function (req, res) {
-  res.sendFile(process.cwd() + 'src/index.html');
+  res.sendFile(process.cwd() + '/src/index.html');
 });
 
 
