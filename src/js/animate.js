e5b11f3abfd493856d347027e115ae5372ede129
Servio Mora <servio.mora19@gmail.com>
2016-11-16 20:17:48 -0500

    added navbar with overlay menu and click events

--- src/js/animate.js
+++ src/js/animate.js
@@ -6,4 +6,34 @@ $(document).ready(function () {
 
   $('.parallax-window').parallax({imageSrc: '/images/stars.jpg'});
 
+  $('#openNav').click(function () {
+    console.log('hit nav btn');
+    $("#mobileNav").css("height", "100%");
+  });
+
+  $('.closebtn').click(function () {
+    /* Close */
+    $("#mobileNav").css("height", "0%");
+  });
+
+  $('.overlay-content a').click(function () {
+    $("#mobileNav").css("height", "0%");
+  });
+  //
+  // $(function(){
+  //   $( ".card .effect-hover" ).bind( "tap", tapHandler );
+  //
+  //   function tapHandler(event){
+  //     $(event.target).addClass( "tap" );
+  //   }
+  // });
+
+  // .card.effect-hover:hover .card-front
+  //   -webkit-transform: rotateY(-180deg)
+  //   transform: rotateY(-180deg)
+  //
+  // .card.effect-hover:hover .card-back
+  //   -webkit-transform: rotateY(0)
+  //   transform: rotateY(0)
+
 });
